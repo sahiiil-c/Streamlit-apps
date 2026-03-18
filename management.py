89324971b0c4d42080d4b3af98192498f1bd01aa
Sahil Chavan <chavansahil2300@gmail.com>
2026-03-18 16:08:39 +0530

    Refactor management.py for improved layout and logo

--- management.py
+++ management.py
@@ -1,25 +1,53 @@
 import streamlit as st
-st.set_page_config(page_title="Ashu's saloon", page_icon="💇", layout="centered")
+st.set_page_config(page_title="Ashu's saloon", page_icon="logo.png", layout="centered")
 
 import pandas as pd
+
+import base64
+
+def get_base64(img_path):
+    with open(img_path, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+img = get_base64("logo.png")
+
+st.markdown(f"""
+    <div style="display:flex; justify-content:center;">
+        <img src="data:image/png;base64,{img}" height="200px" alt="Logo">
+    </div>
+""", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+        .stApp {
+            background: linear-gradient(315deg, #1e3c72, #2a5298,skyblue);
+            background-attachment: flex;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 # Footer
 footer_style = """<style> ... </style>"""
 st.markdown(footer_style, unsafe_allow_html=True)
 
-st.title("Welcome to Ashu's Saloon! 💇‍♂️💇‍♀️")
-
+sidebar=st.sidebar.markdown("")
+sidebar.radio("I am a", ["Admin","Customer"], horizontal=True)
 @st.cache_data
 def load_data(sheet):
     return pd.read_excel("Price_list.xlsx",sheet_name=sheet.lower(),)
 
-serv_select = ["Select","Threading","Waxing","Bleach","Dtan",
+serv_select = ["Select", "dir","Threading","Waxing","Bleach","Dtan",
                "Clean up","Facials","Chemical treatment","Hair spa",
                "Haircut","Hairset"]
 
-serv = st.radio("💄 Choose Service", serv_select,horizontal=True)
-
-if serv != "Select":
+serv = sidebar.radio("💄 Choose Service", serv_select,horizontal=True)
+st.markdown("""
+<style>
+div[data-baseweb="select"] input {
+    display: none;
+}
+</style>
+""", unsafe_allow_html=True)
+if serv != "Select" and serv != "dir":
     df = load_data(serv)
     filtered = df
 
@@ -27,4 +55,7 @@ if serv != "Select":
         st.dataframe(filtered)
     else:
         st.warning("No data found")
+import os
 
+if serv == "dir":
+    st.write(os.listdir())
