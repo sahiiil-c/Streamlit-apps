3e0e1c565b612526aba2dd8f8afb975f47e244fd
Sahil Chavan <chavansahil2300@gmail.com>
2026-03-18 13:28:35 +0530

    Enhance management.py with service selection and footer
    Added footer and service selection functionality to the management system.

--- management.py
+++ management.py
@@ -1,6 +1,33 @@
-import os
 import streamlit as st
+st.set_page_config(page_title="Ashu's saloon", page_icon="💇", layout="centered")
+
 import pandas as pd
 
+# Footer
+footer_style = """<style> ... </style>"""
+st.markdown(footer_style, unsafe_allow_html=True)
 
-st.write("## Ashu's parlour management system")
+st.title("Welcome to Ashu's Saloon! 💇‍♂️💇‍♀️")
+
+@st.cache_data
+def load_data(sheet):
+    return pd.read_excel("Price_list.xlsx",sheet_name=sheet.lower(),)
+
+serv_select = ["Select Service","Threading","Waxing","Bleach","Dtan",
+               "Clean up","Facials","Chemical treatment","Hair spa",
+               "Haircut","Hairset"]
+
+serv = st.selectbox("💄 Choose Service", serv_select)
+
+if serv != "Select Service":
+    df = load_data(serv)
+    filtered = df
+
+    if not filtered.empty:
+        st.dataframe(filtered)
+    else:
+        st.warning("No data found")
+import os
+
+if serv == "Select Service":
+    st.write(os.listdir())
